6252fcfe97477629086011c960260757dd72286b
35jakesmith <35jakesmith@gmail.com>
2022-02-04 20:54:20 -0700

    initialized React app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import About from './componets/About';
+import Nav from './componets/Nav';
+
+function App() {
+  return (
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
